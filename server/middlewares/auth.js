64804a45734e4e9469da5cff81c549e965ee7205
Logan-lv2 <coreecity@gmail.com>
2025-05-18 16:37:33 +0100

    Add files via upload

---
+++ server/middlewares/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const protect = async (req, res, next) => {
+  let token;
+  
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  
+  if (!token) {
+    return res.status(401).json({ 
+      success: false,
+      message: 'Non autorisé - pas de token fourni'
+    });
+  }
+  
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select('-password');
+    next();
+  } catch (err) {
+    return res.status(401).json({ 
+      success: false,
+      message: 'Non autorisé - token invalide'
+    });
+  }
+};
+
+module.exports = { protect };
